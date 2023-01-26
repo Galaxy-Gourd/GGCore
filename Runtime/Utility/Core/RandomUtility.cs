38d7b0b56f84e41355c08390626ab85369edf3e5
Michael Stephens <mstephens.dev@gmail.com>
2023-01-26 17:26:53 -0500

    random utility updates

--- Runtime/Utility/Core/RandomUtility.cs
+++ Runtime/Utility/Core/RandomUtility.cs
@@ -11,6 +11,10 @@ namespace GG.Core
 			return (float) (rand.NextDouble () * (maximum - minimum) + minimum);
 		}
 
+		public static float GetRandomFloatInRange (Vector2 range, System.Random rand) 
+		{
+			return (float) (rand.NextDouble () * (range.y - range.x) + range.x);
+		}
 
 		public static Color GetRandomSeededColor (System.Random random)
 		{
